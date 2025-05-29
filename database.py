6ed28b51216cabb52c71a4859d4d7cd423dc2846
UlviyyaMammadova24 <memmedovaulviyya24@gmail.com>
2025-05-29 12:03:20 +0300

    Connect MongoDB Atlas

--- database.py
+++ database.py
@@ -1,16 +1,17 @@
-# database.py
 from pymongo import MongoClient
-import os
 
-client = MongoClient(os.getenv("MONGO_URI", "mongodb://localhost:27017"))
-db = client["blockchainDB"]
+# MongoDB Atlas bağlantı URI (şifreni <db_password> kısmına yazmalısın)
+client = MongoClient("mongodb+srv://MammadovaMammadova:<db_password>@clusteblockchain.y2cgmgr0.mongodb.net/")
+
+# Veritabanı ve koleksiyon adı
+db = client["blockchain_db"]
 collection = db["blocks"]
 
+# Blok kaydetme fonksiyonu
 def save_block_to_db(block):
     collection.insert_one({
         "index": block.index,
         "data": block.data,
-        "timestamp": block.timestamp,
         "hash": block.hash,
         "previous_hash": block.previous_hash
     })
